06f0a592e79eddb51f37ef5968e3c914601dbc8d
Ruslan6619 <ruslanslobodyan1994@gmail.com>
2023-04-07 17:37:09 +0300

    owl

--- js/scripts.js
+++ js/scripts.js
@@ -87,3 +87,22 @@ function removeSubmenu() {
         }, 500);
     }
 }
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText: [ '', ' ' ],
+
+    responsive:{
+        0:{
+            items:1
+        },
+
+        1000:{
+            items:1
+        }
+    }
+});
